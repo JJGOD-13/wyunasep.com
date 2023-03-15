7a7b4d1e0b32ee1b229150cf1be0ace80d4c971e
JJGOD <jayantgodse13@gmail.com>
2023-03-15 17:10:02 +1100

    small changes

--- app.py
+++ app.py
@@ -15,4 +15,14 @@ def about():
 def contact():
     if request.method == 'GET':
         return render_template('contact.html')
+    if request.method == 'POST':
+        # Check all of the input that is given by the user
+        if not request.form.get('name'):
+            return TODO
+        if not request.form.get('email'):
+            return TODO
+        if not request.form.get('message'):
+            return TODO
+        
+
 
--- static/app.js
+++ static/app.js
@@ -1,3 +1,4 @@
+// Intersection observer to do all the animated text.
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -11,10 +12,12 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el)); 
 
+// document selectors
 let person = document.getElementsByName("name").innerHTML;
 let email = document.getElementsByName("email").innerHTML;
 let message = document.getElementsByName("message").innerHTML;
 
+// Check the persons entries into the form and validate
 function check(){
     if (person == null){
         alert("Please enter your name");
