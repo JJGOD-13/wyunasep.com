c8c82de85381c23dced095a623262b7f591b53a9
JJGOD <jayantgodse13@gmail.com>
2024-07-22 17:33:23 +1000

    carousel links

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -5,28 +5,34 @@ export default function Carousel() {
         <section className=" flex justify-center bg-neutral p-5  ">
             <div >
                 <div className="carousel w-full">
-                    <Link href="">
+                    <Link href="/">
                         <div id="item1" className="carousel-item w-full">
                             <img
                                 src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
                                 className="w-full" />
                         </div>
                     </Link>
-                    <div id="item2" className="carousel-item w-full">
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-                            className="w-full" />
-                    </div>
-                    <div id="item3" className="carousel-item w-full">
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-                            className="w-full" />
-                    </div>
-                    <div id="item4" className="carousel-item w-full">
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-                            className="w-full" />
-                    </div>
+                    <Link href={""}>
+                        <div id="item2" className="carousel-item w-full">
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+                                className="w-full" />
+                        </div>
+                    </Link>
+                    <Link href={""}>
+                        <div id="item3" className="carousel-item w-full">
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+                                className="w-full" />
+                        </div>
+                    </Link>
+                    <Link href={""}>
+                        <div id="item4" className="carousel-item w-full">
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+                                className="w-full" />
+                        </div>
+                    </Link>
                 </div>
                 <div className="flex w-full justify-center gap-2 py-2">
                     <a href="#item1" className="btn btn-xs">1</a>
