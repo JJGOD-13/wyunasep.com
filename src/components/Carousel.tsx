abc609b6dd0839041b4727e0926e8dc05297ec7d
JJGOD <jayantgodse13@gmail.com>
2024-08-13 11:33:50 +1000

    carousel fixes on mobile

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -13,71 +13,51 @@ const goTo = (event: any) => {
 
 export default function Carousel() {
   return (
-    <div className="carousel carousel-center bg-neutral  ">
-      
-        <div className="carousel-item px-10">
-<Link href="/about">
+    <section className=" flex justify-center md:p-10  ">
+      <div className="carousel carousel-center bg-neutral rounded-box max-w-screen space-x-4 p-4 gap-4">
+        <div className="carousel-item">
           <img
             src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-            alt="Pizza"
+            className="rounded-box"
           />
-</Link>
         </div>
-      
-      
-        <div className="carousel-item px-10">
-<Link href="/">
+        <div className="carousel-item">
           <img
             src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
-            alt="Pizza"
+            className="rounded-box"
           />
-</Link>
         </div>
-      
-      
-        <div className="carousel-item px-10">
-<Link href="/">
+        <div className="carousel-item">
           <img
             src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
-            alt="Pizza"
+            className="rounded-box"
           />
-</Link>
         </div>
-      
-      
-        <div className="carousel-item px-10">
-<Link href="/">
+        <div className="carousel-item">
           <img
             src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
-            alt="Pizza"
+            className="rounded-box"
           />
-</Link>
         </div>
-      
-      <div className="carousel-item px-10">
-<Link href="/">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
-          alt="Pizza"
-        />
-</Link>
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
+            className="rounded-box"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
+            className="rounded-box"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+            className="rounded-box"
+          />
+        </div>
       </div>
-      <div className="carousel-item px-10">
-<Link href="/">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
-          alt="Pizza"
-        />
-</Link>
-      </div>
-      <div className="carousel-item px-10">
-<Link href="">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
-          alt="Pizza"
-        />
-</Link>
-      </div>
-    </div>
+    </section>
   );
 }
