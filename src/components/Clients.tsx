153781d83675e73f625c9eeff599d29a6a6bf19d
JJGOD <jayantgodse13@gmail.com>
2024-08-21 11:06:52 +1000

    Fixed some nodejs lsp error

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -7,7 +7,7 @@ import { StaticImport } from "next/dist/shared/lib/get-img-props";
 const folder = "/public/Logos";
 
 export default async function Clients() {
-  const images = require.context("/public/Logos", true);
+  const images = (require as any).context("/public/Logos", true);
   const ImageList = images.keys().map((image: any) => images(image));
   return (
     <section className="  h-1/3 p-10 min-h-56 bg-neutral text-base-content ">
